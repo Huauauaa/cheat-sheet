33c0a48af8ace91f0a640e59c4e871da5b5f550d
Harvey <harvey0379@163.com>
2026-05-22 17:47:15 +0800

    chore: github.com/shopspring/decimal

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module cheat-sheet
 
 go 1.17
+
+require github.com/shopspring/decimal v1.4.0 // indirect
--- main.go
+++ main.go
@@ -1,7 +1,20 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/shopspring/decimal"
+)
 
 func main() {
-	fmt.Println("hello world!")
+	n := 10
+	fmt.Printf("%d\n", n)
+	fmt.Printf("%v\n", n)
+	fmt.Printf("%b\n", n)
+	fmt.Printf("%o\n", n)
+	fmt.Printf("%x\n", n)
+
+	a := 8.2
+	b := 3.8
+	fmt.Println(decimal.NewFromFloat(a).Sub(decimal.NewFromFloat(b)), a-b)
 }
